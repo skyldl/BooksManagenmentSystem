d830bf83186af6f2a6796c3a883a21503a0080aa
LXF <1978772387@qq.com>
2023-06-29 23:49:01 +0800

    Initial commit

---
+++ src/main/java/com/example/bms/BooksManagenmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksManagenmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksManagenmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bms/BooksManagenmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksManagenmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
